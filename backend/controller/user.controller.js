4057b7a0f753e92ab8d75bff1d51f45818c755d9
Ganesh Dashwant <ganeshdashwant04@gmail.com>
2026-03-28 20:17:51 +0530

    fix cookie sameSite and secure for production

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -123,11 +123,15 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
-      //emptied the token hence logged out.
-      message: "Logged out successfully!",
-      success: true,
-    });
+   return res.status(200).cookie("token", "", {
+  maxAge: 0,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+}).json({
+  message: "Logged out successfully!",
+  success: true,
+});
   } catch (error) {
     console.log(error);
   }
